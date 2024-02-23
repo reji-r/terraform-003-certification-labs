b6c2b317a32fbbe8ddb711a9319f0c3367e45fe2
Reji Raveendran Nair <reji.rn.nair@gmail.com>
2024-02-23 18:57:52 +0000

    Example for local-exec execution

---
+++ 011_provisioners_local-exec/main.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "main" {
+  id = "vpc-08fac5af742e1a149"
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "Your_SSH_Public_Key"
+}
+
+resource "aws_instance" "my_server" {
+  ami                    = "ami-0440d3b780d96b29d"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.sg_my_server.id]
+  
+  provisioner "local-exec" {
+    command    = "echo ${self.private_ip} >> private_ips.txt"
+    on_failure = continue
+  }
+
+  tags = {
+    Name = "MyAppServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
+}
+
+resource "aws_security_group" "sg_my_server" {
+  name        = "sg_my_server"
+  description = "MyAppServer security group"
+  vpc_id      = data.aws_vpc.main.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.sg_my_server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+  description       = "HTTP"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.sg_my_server.id
+  cidr_ipv4         = "YOUR_IP_ADDRESS"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  description       = "SSH"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_my_server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.sg_my_server.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
